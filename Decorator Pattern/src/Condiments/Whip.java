0b481d013ee0af592e4428b600659ad29d8fb655
Edisson David Prieto Vargas <v-edivargas@expediagroup.com>
2023-01-05 00:27:27 -0500

    Decorator pattern
    Implementation of Coffee Station to apply this pattern and see how it works.

---
+++ Decorator Pattern/src/Condiments/Whip.java
@@ -0,0 +1,18 @@
+package Condiments;
+
+import Beverages.Beverage;
+
+public class Whip extends CondimentDecorator {
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+}
+
